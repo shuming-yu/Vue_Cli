f04711509c62565cfad498b14939607146b3f601
ben <yu.shuming0829@gmail.com>
2022-04-20 23:10:25 +0800

    cli .env

--- new-product/src/App.vue
+++ new-product/src/App.vue
@@ -4,9 +4,23 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  {{ name }}
   <button type="button" class="btn btn-primary">Primary</button>
 </template>
 
 <style lang="scss">
 @import 'bootstrap';
 </style>
+<!-- env 參考 : https://cli.vuejs.org/zh/guide/mode-and-env.html -->
+<script>
+export default {
+  data() {
+    return {
+      name: process.env.VUE_APP_NAME
+      // process 固定名稱
+      //     機密資訊-存本機   正式環境         測試環境
+      // .env  .env.local  .env.production  .env.development  .env.development.local
+    }
+  }
+}
+</script>
--- new-product/src/main.js
+++ new-product/src/main.js
@@ -1,21 +1,23 @@
 import { createApp } from 'vue'
-import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
-import { required, email, min } from '@vee-validate/rules'
-import { localize, setLocale } from '@vee-validate/i18n'
-import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate' // 匯入 vee-validate 主套件
+import { required, email, min } from '@vee-validate/rules' // 匯入 vee-validate 相關規則
+import { localize, setLocale } from '@vee-validate/i18n' // 匯入多國語系的功能
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json' // 匯入繁體中文語系檔案
 
 import App from './App.vue'
 import router from './router'
 
+// 定義驗證規則
 defineRule('required', required)
 defineRule('email', email)
 defineRule('min', min)
 
+// 設定 vee-validate 全域規則
 configure({
   generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
   validateOnInput: true // 當輸入任何內容直接進行驗證
 })
-setLocale('zh_TW')
+setLocale('zh_TW') // 設定預設語系
 
 const app = createApp(App).use(router)
 
