4c7587c2a341b27466d153750159638bc8e4f4a2
ben <yu.shuming0829@gmail.com>
2022-04-24 21:45:56 +0800

    config vue.config.js publicPath

--- new-product/vue.config.js
+++ new-product/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/dist/',
+  publicPath: '/new-product/dist/',
   lintOnSave: false
 })
